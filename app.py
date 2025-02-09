a4d399a914174ed5c6a2d9a577f8ca651655797a
Zainab Aamir <zaamir427@gmail.com>
2025-02-09 21:52:35 +0500

    ..

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///todo.db"
@@ -7,7 +8,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False
 db = SQLAlchemy(app)
 
 class todo(db.model):
-    
+    id = db.Column(db.integer, primary_key= True)
+    title = db.Column(db.String(200), nullable = False)
+    description = db.Column(db.String(500), nullable= False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
 @app.route("/")
 def hello_world():
